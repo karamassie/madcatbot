72eebbe8a0f36234c641eea1ac384d87235797ee
Kara Massie <kara@hackclub.com>
2022-12-01 10:03:54 -0500

    updating timing to be less frequent

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ const app = new App({
   port: process.env.PORT || 3000
 });
 
-// let sanityCheck = async () => {
-//   await app.client.chat.postMessage({
-//     channel: 'C039E59328Y',
-//     text: "hellooooo (in)sanity"
-//   });
-// }
-// sanityCheck()
+
+
+
+
+
 
 // Listens to incoming messages that contain "kitty kitty kitty
 app.message('kitty kitty kitty', async ({ message, say }) => {
@@ -81,12 +79,14 @@ app.message('WHY', async ({ message, say }) => {
 
 //const welcomeChannelId = 'C039E59328Y';
 
+
+
 const theLitterBox = 'C039E59328Y'
-const numerology = 'C04207S1PQX'
+
 
 const randomChannel = () => {
   const n = Math.random()
-  if (n > 0.005) {
+  if (n > 0.001) {
     return theLitterBox
   } else {
     return numerology
@@ -188,7 +188,7 @@ app.message(async ({ message, say }) => {
 
 //random actions at set intervals in channel
 
-const randomCatActions = ["_bats at the CNC machine_", "_sniffs @ian's shoes_", "_knocks over the waterbowl_", "_farts, then stares at the dog blamefully_", "_grumps_", "_sighs_", "_eyes a pair of trousers it wants to poke holes in_", "_perches on a windowsill_", "_leaves a cat-hair donut-stain on a fancy cushion_", "_follows @ella around, but at a safe distance_", "_begs to go outside_", "_is deeply suspicious of other channels_", "_lives in a constant state of ennui_", "_jumps at a shadow that moved_", "_narrows its eyes at @caleb_", "_overeats_" , "_leaves pockmarks in zach's favorite chair_", "_flops on its side in exasperation_","_gets under @belle's feet_","_is guided by 5: the claws on its front paw and the number of meals it believes to need each day_","_longs for @celeste_", "_scratched o  the  .  key_", "_is suspiciously cute_", "_eyes @ishan's cheese_","_flexes its claws while yawning_", "_stays clear of rolling office chairs_"]
+const randomCatActions = ["_bats at the CNC machine_", "_sniffs @ian's shoes_", "_knocks over the waterbowl_", "_farts, then stares at the dog blamefully_", "_grumps_", "_sighs_", "_eyes a pair of trousers it wants to poke holes in_", "_perches on a windowsill_", "_leaves a cat-hair donut-stain on a fancy cushion_", "_follows @ella around, but at a safe distance_", "_begs to go outside_", "_is deeply suspicious of other channels_", "_lives in a constant state of ennui_", "_jumps at a shadow that moved_", "_narrows its eyes at @caleb_", "_overeats_" , "_leaves pockmarks in zach's favorite chair_", "_flops on its side in exasperation_","_gets under @belle's feet_","_is guided by 5: the claws on its front paw and the number of meals it believes to need each day_","_leggos @sampoder's eggos_", "_scratched o  the  .  key_", "_is suspiciously cute_", "_eyes @ishan's cheese_","_flexes its claws while yawning_", "_stays clear of rolling office chairs_"]
 
 //const randomCatActions = ["_pretends not to like celeste_","_sheds_","_supermans on the cool tiles_","_clogs the laptop fan with ha*0+ir#LQ#@)FKDAs_","_longs to go out_","_stares at a pixie in the corner_","_judges_","_blurs after a dustbunny_", "_rubs against @ella's bike_","_STOMPS_","_stares woefully at its empty dish_","_was last seen hanging by its claws from a curtain_","_stares at @hugo while they're sleeping_","_scowls at The World_","_lies on important papers_","_snores far too loudly for a cat its size_","_trails litter everywhere_","_parkours over Caleb's laptop_","_sheds_","_is hiding under the sofa_","_is sleeping on Jacob's jacket_"]
 
@@ -200,7 +200,7 @@ console.log('check if randomness is running');
     channel: randomChannel(),
     text: randomCatActions[x]
   });
-}, 2000 * 1000);
+}, 1000 * 1000);
 
 (async () => {
   await app.start();
